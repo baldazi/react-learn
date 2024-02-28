ee56ae8c28c4f144c96b8e8f7695e43ad9c2199b
Abdoul Aziz BALDE <59298812+baldazi@users.noreply.github.com>
2024-02-28 18:15:54 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload from here.
           here text added
+          <p>Thanx</p>
         </p>
         <a
           className="App-link"
