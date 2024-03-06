b8d88cae888f7dfa5e4acb3e66a7de766f6977be
Abdoul Aziz BALDE <59298812+baldazi@users.noreply.github.com>
2024-03-06 23:36:54 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React And Github page
         </a>
       </header>
     </div>
